a4894561a3708d70261485df498bb02c7536cb0d
Veregorn <rjbarco@gmail.com>
2024-04-16 10:44:47 +0200

    Form created and linked to be able to send new messages to the board

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Veregorn Mini Message Board', messages: messages });
 });
 
+/* Adding a router post method */
+router.post('/new', function(req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: new Date()
+  });
+  res.redirect('/');
+});
+
 module.exports = router;
