1e99222c991262c24c75967eb55887a5bed9f073
Veregorn <rjbarco@gmail.com>
2024-04-16 10:00:15 +0200

    Added 'messages' array and display it in the views

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Veregorn",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Aragorn",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Veregorn Mini Message Board', messages: messages });
 });
 
 module.exports = router;
